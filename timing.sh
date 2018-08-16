b33d977321f8bbd800bc7542ca582d223d4a19b7
Tobias Doernbach <t.fromm@jacobs-university.de>
2018-08-16 13:48:06 +0200

    updated timing scripts

--- timing.sh
+++ timing.sh
@@ -2,19 +2,11 @@
 
 # outputs the runtimes into runtimes.csv, format: exit status, wall time, system time, user time
 
-runtimes_file=../logs/runtimes.csv
-world_file=""
-
-if [ "$1" == "-w" ]; then
-    world_file=$2
-fi
+container=$1
+world_file=$2
+runtimes_file=../logs/runtimes_$container.csv
 if [ ! -f $runtimes_file ]; then
     echo -e "world_file,exit_code,wall_time,system_time,user_time" > $runtimes_file
 fi
-if [ "$world_file" == "" ]; then
-    echo -e ",\c" >> $runtimes_file
-    /usr/bin/time -f %x,%e,%S,%U --quiet -a -o $runtimes_file "${@:1}"
-else
-    echo -e "$world_file,\c" >> $runtimes_file
-    /usr/bin/time -f %x,%e,%S,%U --quiet -a -o $runtimes_file "${@:3}"
-fi
+echo -e "$world_file,\c" >> $runtimes_file
+/usr/bin/time -f %x,%e,%S,%U --quiet -a -o $runtimes_file "${@:3}"
